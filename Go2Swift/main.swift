418c555a6b4b8b9ac366a7509114ce094f9e4c82
1l0 <rh.plus001@gmail.com>
2017-09-07 21:58:20 +0900

    hmm

--- Go2Swift/main.swift
+++ Go2Swift/main.swift
@@ -1,17 +1,19 @@
 //import Foundation
 import GoEcho
 
-func GoStr(s: String) -> GoString {
-    return GoString(p:s, n:GoInt(s.characters.count))
+func GoStr(_ s: String) -> GoString {
+    print(s.utf8.count) // need for some reasons we don't care
+    return GoString(p:s, n:GoInt(s.utf8.count))
 }
-func GoStrVal(s: GoString) -> String {
+func SwiStr(_ s: GoString) -> String {
     if let ss = String(validatingUTF8: s.p){
         return ss
     }
     return ""
+    //return String(cString: s.p)
 }
-let s = GoStr(s:"hello")
-print(GoStrVal(s:EchoString(s)))
+let s = GoStr("hello")
+print(SwiStr(EchoString(s)))
 let int = GoInt(1)
 print(EchoInt(int))
 let fl = GoFloat64(0.1)
